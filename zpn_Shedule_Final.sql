fed7f4a24dc87c487c9ac38d655baa66330da4a1
AlexeyRaid <la@zoolux.com.ua>
2023-03-04 22:38:47 +0200

    All pay conditions
    Signed-off-by: AlexeyRaid <la@zoolux.com.ua>

--- zpn_Shedule_Final.sql
+++ zpn_Shedule_Final.sql
@@ -19,7 +19,24 @@ select gr.fio,
        pay.team_share_of_assign_pharmacy_above_prem_threshold as БрНазнАптекаПремия,
        pay.team_share_of_used_pharmacy_above_prem_threshold   as БрИспАптекаПремия,
        pay.team_norm_pharmacy_mid_shift                       as БрНормаАптекиСрСмена,
-       pay.team_norm_pharmacy_by_shift                        as БрНормаАптеки_в_Смену
+       pay.team_norm_pharmacy_by_shift                        as БрНормаАптеки_в_Смену,
+       pay.team_rate                                          as БрСтавка,
+       pay.team_mid_in_month_gar                              as БрСрМесГар,
+       pay.team_in_shift_gar                                  as БрГарСм,
+       pay.team_diff_percent                                  as Перикос_Бригадного_,
+       pay.pers_of_serv_assign                                as ПерсНазУсл,
+       pay.pers_of_serv_used                                  as ПерсИспУсл,
+       pay.pers_of_prescribed_med                             as ПерсНазМед,
+       pay.pers_of_used_med                                   as ПерсИспМед,
+       pay.pers_of_assign_pharmacy_prem_under_limit           as ПерсНазнАптекаДоЛимита,
+       pay.pers_of_used_pharmacy_prem_under_limit             as ПерсИспАптекаДоЛимита,
+       pay.pers_of_assign_pharmacy_above_prem_threshold       as ПерсНазнАптекаПремия,
+       pay.pers_of_used_pharmacy_above_prem_threshold         as ПерсИспАптекаПремия,
+       pay.pers_norm_pharmacy_mid_shift                       as НормаАптекиСрСмена,
+       pay.pers_norm_pharmacy_by_shift                        as НормаАптеки_в_Смену,
+       pay.pers_rate                                          as ПерсСтавка,
+       pay.pers_mid_in_month_gar                              as ПерсСрМесГар,
+       pay.pers_in_shift_gar                                  as ПерсГарСм
 
 
 from analyticdb.zpn_sched_lev_uniqp as gr
