e4e4d305daa717a2765ccd260a6614b21a8b162f
AlexeyRaid <la@zoolux.com.ua>
2023-03-07 09:42:52 +0200

    Добавлены смены коректировки (тест)
    Signed-off-by: AlexeyRaid <la@zoolux.com.ua>

--- График.sql
+++ График.sql
@@ -1,28 +1,29 @@
- select gr.clinic,
-        gr.department,
-        gr.post,
-        gr.role,
-        gr.shift,
-        gr.employee,
-        emp.fio                                                                         as fio,
+use analyticdb;
+select gr.clinic,
+       gr.department,
+       gr.post,
+       gr.role,
+       gr.shift,
+       gr.employee,
+       emp.fio                                           as fio,
 
 -- Формируем дату смены с числовых значений
-        date(concat(gr.year, "-", gr.month, "-", gr.day))                               as DateSm,
+       date(concat(gr.year, "-", gr.month, "-", gr.day)) as DateSm,
 
-     /*      -- Уник роли смены (без даты) для коректировок
-           concat(gr.clinic, "_", gr.department, "_", gr.post, "_", gr.role, "_", gr.shift) as UnicAdjastm,
-    */
+    /*      -- Уник роли смены (без даты) для коректировок
+          concat(gr.clinic, "_", gr.department, "_", gr.post, "_", gr.role, "_", gr.shift) as UnicAdjastm,
+   */
 -- Формируем датувремя Смены с учетом корректировки
-        -- ДатаВремя начала смены
-        case
+       -- ДатаВремя начала смены
+       case
            when kor.time_start is null
                then -- если корректировки нет - вносим данные с графика, если корректировка есть - тогда подменяем данные корректировкой
                cast(concat(date(concat(gr.year, "-", gr.month, "-", gr.day)), " ", gr.time_start) as datetime)
            else
                cast(concat(kor.shift_date, " ", kor.time_start) as datetime)
-           END                                                                          as DTStart,
-        -- Формируем дату конца смены, если смена переходит на другой день - добавляем сутки к дате начала смены
-        case
+           END                                           as DTStart,
+       -- Формируем дату конца смены, если смена переходит на другой день - добавляем сутки к дате начала смены
+       case
            when kor.time_end is null
                then -- если корректировки нет - вносим данные с графика, если корректировка есть - тогда подменяем данные корректировкой
                case
@@ -40,10 +41,10 @@
                    else
                            cast(concat(kor.shift_date, " ", kor.time_end) as datetime) + INTERVAL 1 DAY
                    END
-           END                                                                          as DTEnd,
+           END                                           as DTEnd,
 
 -- Вычисляем длительность смены, с учетом корректировки
-        timediff(
+       timediff(
                case
                    when kor.time_end is null
                        then -- если корректировки нет - вносим данные с графика, если корректировка есть - тогда подменяем данные корректировкой
@@ -72,12 +73,12 @@
                    else
                        cast(concat(kor.shift_date, " ", kor.time_start) as datetime)
                    END)
-                                                                                        as DlSmen,
+                                                         as DlSmen,
 
 -- Вставляем метку, если смена корректированная
-        kor.time_start is not null                                                       as Korr
+       kor.time_start is not null                        as Korr
 
- from analyticdb.gs_schedule_tmp as gr
+from analyticdb.gs_schedule_tmp as gr
 
 -- Тянем смены корректировки.
          left join analyticdb.gs_correction_tmp as kor
@@ -89,5 +90,20 @@
 -- Подставляем полное имя сотрудника
          left join gs_employee as emp on gr.employee = emp.fio_schedule
 
- where gr.employee is not null
+where gr.employee is not null
   and gr.month = 2
+
+UNION
+select cor.clinic,
+       cor.department,
+       cor.post,
+       cor.role,
+       cor.shift,
+       cor.employee,
+       cor.fio,
+       cor.shift_date,
+       cor.DTStart,
+       cor.DTEnd,
+       cor.DlSmen,
+       '1'
+from zpn_correction as cor
