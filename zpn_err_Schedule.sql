016de919a686e21624eaa6e74ad3e770ed7eda16
AlexeyRaid <la@zoolux.com.ua>
2023-03-08 11:35:21 +0200

    group
    Signed-off-by: AlexeyRaid <la@zoolux.com.ua>

--- zpn_err_Schedule.sql
+++ zpn_err_Schedule.sql
@@ -1,4 +1,12 @@
-select gr.employee, gr.fio, gr.level, gr.department, gr.DateSm
+select gr.employee,
+       gr.fio,
+       gr.post,
+       gr.level,
+       gr.department,
+       min(gr.DateSm) as Date_From,
+       max(gr.DateSm) as Date_To
 from analyticdb.zpn_sched_lev as gr
 where gr.fio is null
    or gr.level is null
+   or gr.post
+group by gr.employee, gr.fio, gr.post, gr.level, gr.department
