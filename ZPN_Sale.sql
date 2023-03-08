798d31c42a21256ced61a677d52e37bc7da2e09f
AlexeyRaid <la@zoolux.com.ua>
2023-03-08 15:27:14 +0200

    add sal analyt

--- ZPN_Sale.sql
+++ ZPN_Sale.sql
@@ -3,15 +3,21 @@ select s.period,
        s.employee,
        s.executor,
        s.nomenclature_key,
+       anal.description,
+       nom.code,
        s.price_without_discounts,
        s.price,
        s.amount_of_costs
 from analyticdb.et_sales as s
 
--- Подтягиваем Клинику, как в графике
+-- Подтягиваем тип продажи
+         left join analyticdb.et_nomenclature as nom on s.nomenclature_key = nom.ref_key
+         left join analyticdb.et_salary_analytics as anal on nom.salary_analytics_key = anal.ref_key
 
 
 where s.period between '2023-02-01' and '2023-02-28'
   and (s.price <> 0
     or s.amount_of_costs <> 0
     or s.price_without_discounts <> 0)
+  and anal.description is null
+  and nom.is_folder = 0
