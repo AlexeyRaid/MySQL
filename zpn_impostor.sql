3fa97062973f22bd3317889ac2b35f4c26347d85
AlexeyRaid <la@zoolux.com.ua>
2023-03-10 01:05:06 +0200

    not work

--- zpn_impostor.sql
+++ zpn_impostor.sql
@@ -16,6 +16,12 @@ select s.recorder,
        pay.pers_of_assign_pharmacy_prem_under_limit as ПерсНазнАптекаДоЛимита,
        pay.pers_of_used_pharmacy_prem_under_limit   as ПерсИспАптекаДоЛимита
 
+#        pay.clinic,
+#        pay.post,
+#        pay.role,
+#        pay.level,
+#        pay.department
+
 from analyticdb.et_sales as s
 
 -- Подтягиваем тип продажи
@@ -34,8 +40,19 @@ from analyticdb.et_sales as s
          left join analyticdb.zpn_payconditions as pay on s.period between pay.date_from and pay.date_to and
                                                           pay.role = 'Самозванец' and
                                                           cl.clinic = pay.clinic and
-                                                          lev.level = pay.level and
-                                                          pay.post = lev.post
+                                                          pay.post = lev.post and
+                                                          pay.level =
+                                                          (select lev.level
+                                                           from analyticdb.zpn_levelsemployees as lev
+                                                                    left join analyticdb.gs_employee as em on lev.fio = em.fio
+                                                                    left join analyticdb.et_sales as s
+                                                                              on s.period between lev.date_from and lev.date_to
+                                                                                  and em.ref_key = s.employee
+                                                           where s.period >= '2023-02-01 00:00'
+#                                                              and em.fio_schedule = 'Гайворонський'
+                                                           group by lev.fio, lev.level
+                                                           order by lev.level
+                                                           limit 1)
 
 where s.period between '2023-02-01' and '2023-02-28'
   and (s.price <> 0
@@ -51,4 +68,7 @@ where s.period between '2023-02-01' and '2023-02-28'
   and anal.ref_key is not null
   and gr.DateSm is null
   and emp.ref_key is not null
+
+  and s.recorder = '014643ae-b040-11ed-7386-e607dc9b591c'
+  and s.line_number = 1
 
