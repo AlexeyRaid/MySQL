1b283da20f341a9676df4f5da9efcc83c85b48da
AlexeyRaid <la@zoolux.com.ua>
2023-03-09 20:29:26 +0200

    upd Отключена подтяжка с графика

--- zpn_sale_apt_nazn.sql
+++ zpn_sale_apt_nazn.sql
@@ -5,10 +5,10 @@ select s.period,
        s.price_without_discounts as price_with_disc,
        s.price,
        s.amount_of_costs,
-       gr.БрНазнАптекаДоЛимита,
-       gr.БрНазнАптекаПремия,
-       gr.ПерсНазнАптекаДоЛимита,
-       gr.ПерсНазнАптекаПремия
+--       gr.БрНазнАптекаДоЛимита,
+--       gr.БрНазнАптекаПремия,
+--       gr.ПерсНазнАптекаДоЛимита,
+--       gr.ПерсНазнАптекаПремия
 
 from analyticdb.et_sales as s
 
@@ -17,14 +17,15 @@ from analyticdb.et_sales as s
          left join analyticdb.et_salary_analytics as anal on nom.salary_analytics_key = anal.ref_key
 
 -- Тянем график с условиями
-         left join analyticdb.zpn_sched_conditions_final as gr on s.period between gr.DTStart and gr.DTEnd and
-                                                                  s.employee = gr.fio_ref
-where s.period between '2023-02-01' and '2023-02-28'
+--         left join analyticdb.zpn_sched_conditions_final as gr on s.period between gr.DTStart and gr.DTEnd and
+    s.employee = gr.fio_ref
+where s.period between '2023-02-01'
+  and '2023-02-28'
   and (s.price <> 0
-    or s.amount_of_costs <> 0
-    or s.price_without_discounts <> 0)
+   or s.amount_of_costs <> 0
+   or s.price_without_discounts <> 0)
   and nom.is_folder = 0
   and anal.ref_key is not null
-  and (gr.БрНазнАптекаДоЛимита is not null or gr.БрНазнАптекаПремия is not null or
-       gr.ПерсНазнАптекаДоЛимита is not null or gr.ПерсНазнАптекаПремия is not null)
+--  and (gr.БрНазнАптекаДоЛимита is not null or gr.БрНазнАптекаПремия is not null or
+--       gr.ПерсНазнАптекаДоЛимита is not null or gr.ПерсНазнАптекаПремия is not null)
   and anal.description = 'Аптека+Зоомагазин'
