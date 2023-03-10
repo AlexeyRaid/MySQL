5ba19f855081bcc77e683bda59789d3c51577156
AlexeyRaid <la@zoolux.com.ua>
2023-03-10 10:54:59 +0200

    final?

---
+++ zpn_zavotd_Nazn.sql
@@ -0,0 +1,24 @@
+select s.period,
+       s.employee,
+       s.price,
+       pay.recipient,
+       pay.share_donor_assigned_services
+from analyticdb.et_sales as s
+
+         left join analyticdb.zpn_sched_conditions_final as graf on s.period between graf.DTStart and graf.DTEnd and
+                                                                    s.employee = graf.fio_ref
+         left join analyticdb.zpn_paycond_zavotd as pay on s.period between pay.date_from and pay.date_to
+    and s.employee = pay.ref_donor
+    and pay.department = graf.department
+
+-- Подтягиваем тип продажи
+         left join analyticdb.et_nomenclature as nom on s.nomenclature_key = nom.ref_key
+         left join analyticdb.et_salary_analytics as anal on nom.salary_analytics_key = anal.ref_key
+
+
+where s.period >= '2023-02-01 00:00'
+  and pay.share_donor_assigned_services is not null
+  and (s.price <> 0
+    or s.amount_of_costs <> 0
+    or s.price_without_discounts <> 0)
+  and anal.description = 'Клиника'
