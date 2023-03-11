34f7d019b089367a3cee0cce3092ff60b222457b
AlexeyRaid <la@zoolux.com.ua>
2023-03-11 12:11:40 +0200

    Убрал фильтр по нулевым значениям

--- zpn_zavotd_isp.sql
+++ zpn_zavotd_isp.sql
@@ -1,19 +1,19 @@
 select s.period,
        s.recorder,
        s.line_number,
-       s.employee,
+       s.executor,
        s.price,
        s.price_without_discounts,
        s.amount_of_costs,
        pay.recipient,
-       pay.share_donor_assigned_services
+       pay.share_donor_used_services
 from analyticdb.et_sales as s
 
          left join analyticdb.zpn_sched_conditions_final as graf
                    on s.period >= graf.DTStart and s.period <= graf.DTEnd and
-                      s.employee = graf.fio_ref
+                      s.executor = graf.fio_ref
          left join analyticdb.zpn_paycond_zavotd as pay on s.period >= pay.date_from and s.period <= pay.date_to
-    and s.employee = pay.ref_donor
+    and s.executor = pay.ref_donor
     and pay.department = graf.department
 
 -- Подтягиваем тип продажи
@@ -22,7 +22,7 @@ from analyticdb.et_sales as s
 
 
 where s.period >= '2023-02-01 00:00'
---  and pay.share_donor_assigned_services is not null
+--  and pay.share_donor_used_services is not null
   and (s.price <> 0
     or s.amount_of_costs <> 0
     or s.price_without_discounts <> 0)
