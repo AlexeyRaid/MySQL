81d10038f38befc1313ead9fd12b9385d5f70b44
AlexeyRaid <la@zoolux.com.ua>
2023-03-11 15:29:18 +0200

    add qty

--- zpn_odnoraz_nazn.sql
+++ zpn_odnoraz_nazn.sql
@@ -1,4 +1,4 @@
-select s.period, s.employee, s.nomenclature_key, s.price, dis.share_of_check
+select s.period, s.employee, s.nomenclature_key, s.quantity_paid, s.price, dis.share_of_check
 from analyticdb.zpn_paydisposable as dis
 
          left join analyticdb.et_sales as s on dis.ref_key = s.nomenclature_key and
