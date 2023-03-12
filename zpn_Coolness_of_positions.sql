0600dd72207b28ac65c7c967ae600c3f6860bcf8
AlexeyRaid <la@zoolux.com.ua>
2023-03-12 21:46:26 +0200

    finish Наконец-то!!!!

--- zpn_Coolness_of_positions.sql
+++ zpn_Coolness_of_positions.sql
@@ -1,4 +1,7 @@
-SELECT zpn.fio, MAX(p.priority) AS priority, p.post, p.level, zpn.date_from, zpn.date_to
+SELECT zpn.fio, MAX(p.priority) AS priority, p.post, p.level -- , zpn.date_from, zpn.date_to
 FROM zpn_levelsemployees zpn
          LEFT JOIN gs_posts AS p ON p.post = zpn.post AND p.level = zpn.level
+where '2023-02-02' between zpn.date_from and zpn.date_to
+
 GROUP BY zpn.fio
+
