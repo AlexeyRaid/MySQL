9e5a37bf4da4231ec52f338efa528e7503117129
AlexeyRaid <la@zoolux.com.ua>
2023-03-17 21:54:51 +0200

    add post

--- zpn_odnoraz_nazn.sql
+++ zpn_odnoraz_nazn.sql
@@ -1,7 +1,29 @@
-select s.period, s.employee, s.nomenclature_key, s.quantity_paid, s.price, dis.share_of_check
+select s.period,
+       s.employee,
+       s.organization_key,
+       s.nomenclature_key,
+       s.quantity_paid,
+       s.price,
+       dis.share_of_check,
+       ifnull(gr.post, post.post) as post
+
 from analyticdb.zpn_paydisposable as dis
 
          left join analyticdb.et_sales as s on dis.ref_key = s.nomenclature_key and
                                                s.period >= dis.date_from and s.period <= dis.date_to
+
+         left join analyticdb.zpn_schedule as gr on s.period >= gr.DTStart and s.period < gr.DTEnd
+
+         RIGHT JOIN analyticdb.gs_employee AS em ON em.ref_key = s.employee
+
+         left join analyticdb.gs_posts as post on
+        (SELECT l.priority
+         FROM analyticdb.zpn_levelsemployees AS l
+         WHERE em.fio = l.fio
+           AND s.period >= l.date_from
+           AND s.period <= l.date_to
+         ORDER BY l.priority desc
+         LIMIT 1) = post.priority
+
 where s.period >= '2023-02-01 00:00'
   and s.price <> 0
