cd0bae2b689b1f2d4720b976f6fcbd05c5710593
AlexeyRaid <la@zoolux.com.ua>
2023-03-23 00:11:35 +0200

    new

---
+++ Продажи по группам/sale_misgroup.sql
@@ -0,0 +1,15 @@
+select date(s.period)              as Date,
+       s.organization_key          as Clinic,
+       n.description               as Nomenclature,
+       n.code                      as Code,
+       n.deletion_mark             as Del,
+       s.price                     as Summ,
+       s.price_without_discounts   as SuumWODisc,
+       s.price - s.amount_of_costs as Profit
+from analyticdb.et_sales as s
+         left join analyticdb.et_nomenclature as n on n.ref_key = s.nomenclature_key
+         left join analyticdb.et_salary_analytics as a on a.ref_key = n.salary_analytics_key
+where (s.price <> 0
+    or s.amount_of_costs <> 0
+    or s.price_without_discounts <> 0)
+  and a.description is null
