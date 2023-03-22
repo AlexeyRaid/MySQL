16115666cdbda342c17cd83b721fb8515f7fc539
AlexeyRaid <la@zoolux.com.ua>
2023-03-22 13:37:42 +0200

    Переделал на новый формат дат

--- График.sql
+++ График.sql
@@ -1,38 +1,33 @@
-use analyticdb;
 select gr.clinic,
        gr.department,
        gr.post,
        gr.role,
        gr.shift,
        gr.employee,
-       emp.fio                                           as fio,
+       emp.fio                                            as fio,
 
 -- Формируем дату смены с числовых значений
-       date(concat(gr.year, "-", gr.month, "-", gr.day)) as DateSm,
+       date_add(gr.year_month, INTERVAL (gr.day - 1) day) as DateSm,
 
-    /*      -- Уник роли смены (без даты) для коректировок
-          concat(gr.clinic, "_", gr.department, "_", gr.post, "_", gr.role, "_", gr.shift) as UnicAdjastm,
-   */
 -- Формируем датувремя Смены с учетом корректировки
        -- ДатаВремя начала смены
        case
            when kor.time_start is null
                then -- если корректировки нет - вносим данные с графика, если корректировка есть - тогда подменяем данные корректировкой
-               cast(concat(date(concat(gr.year, "-", gr.month, "-", gr.day)), " ", gr.time_start) as datetime)
+               cast(concat((date_add(gr.year_month, INTERVAL (gr.day - 1) day)), " ", gr.time_start) as datetime)
            else
                cast(concat(kor.shift_date, " ", kor.time_start) as datetime)
-           END                                           as DTStart,
+           END                                            as DTStart,
        -- Формируем дату конца смены, если смена переходит на другой день - добавляем сутки к дате начала смены
        case
            when kor.time_end is null
                then -- если корректировки нет - вносим данные с графика, если корректировка есть - тогда подменяем данные корректировкой
                case
                    when gr.time_start < gr.time_end then
-                       cast(concat(date(concat(gr.year, "-", gr.month, "-", gr.day)), " ",
-                                   gr.time_end) as datetime)
+                       cast(concat(date_add(gr.year_month, INTERVAL (gr.day - 1) day), " ", gr.time_start) as datetime)
                    else
-                           cast(concat(date(concat(gr.year, "-", gr.month, "-", gr.day)), " ",
-                                       gr.time_end) as datetime) + INTERVAL 1 DAY
+                           cast(concat(date_add(gr.year_month, INTERVAL (gr.day - 1) day), " ",
+                                       gr.time_start) as datetime) + INTERVAL 1 DAY
                    END
            else
                case
@@ -41,7 +36,7 @@ select gr.clinic,
                    else
                            cast(concat(kor.shift_date, " ", kor.time_end) as datetime) + INTERVAL 1 DAY
                    END
-           END                                           as DTEnd,
+           END                                            as DTEnd,
 
 -- Вычисляем длительность смены, с учетом корректировки
        timediff(
@@ -50,11 +45,11 @@ select gr.clinic,
                        then -- если корректировки нет - вносим данные с графика, если корректировка есть - тогда подменяем данные корректировкой
                        case
                            when gr.time_start < gr.time_end then
-                               cast(concat(date(concat(gr.year, "-", gr.month, "-", gr.day)), " ",
-                                           gr.time_end) as datetime)
+                               cast(concat(date_add(gr.year_month, INTERVAL (gr.day - 1) day), " ",
+                                           gr.time_start) as datetime)
                            else
-                                   cast(concat(date(concat(gr.year, "-", gr.month, "-", gr.day)), " ",
-                                               gr.time_end) as datetime) + INTERVAL 1 DAY
+                                   cast(concat(date_add(gr.year_month, INTERVAL (gr.day - 1) day), " ",
+                                               gr.time_start) as datetime) + INTERVAL 1 DAY
                            END
                    else
                        case
@@ -68,21 +63,20 @@ select gr.clinic,
                case
                    when kor.time_start is null
                        then -- если корректировки нет - вносим данные с графика, если корректировка есть - тогда подменяем данные корректировкой
-                       cast(concat(date(concat(gr.year, "-", gr.month, "-", gr.day)), " ",
-                                   gr.time_start) as datetime)
+                       cast(concat(date_add(gr.year_month, INTERVAL (gr.day - 1) day), " ", gr.time_start) as datetime)
                    else
                        cast(concat(kor.shift_date, " ", kor.time_start) as datetime)
                    END)
-                                                         as DlSmen,
+                                                          as DlSmen,
 
 -- Вставляем метку, если смена корректированная
-       kor.time_start is not null                        as Korr
+       kor.time_start is not null                         as Korr
 
 from analyticdb.gs_schedule_tmp as gr
 
 -- Тянем смены корректировки.
          left join analyticdb.gs_correction_tmp as kor
-                   on (date(concat(gr.year, "-", gr.month, "-", gr.day))) = kor.shift_date and
+                   on (date_add(gr.year_month, INTERVAL (gr.day - 1) day)) = kor.shift_date and
                       (concat(gr.clinic, "_", gr.department, "_", gr.post, "_", gr.role, "_", gr.shift)) =
                       (concat(kor.clinic, "_", kor.department, "_", kor.post, "_", kor.role, "_", kor.shift)) and
                       gr.employee = kor.employee
