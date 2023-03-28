96179de4130144c8497647e6609330cd8eca3e83
AlexeyRaid <la@zoolux.com.ua>
2023-03-28 06:35:40 +0300

    новая логика от Одегова

--- zpn_double_overlapsSched.sql
+++ zpn_double_overlapsSched.sql
@@ -1,8 +1,4 @@
-SELECT gr1.DateSm,
-       gr2.DateSm,
-
-
-       gr1.Korr,
+SELECT gr1.DateSm                             as Kl1_DateSm,
        gr1.clinic                             as Kl1,
        gr1.employee                           as Kl1_Sotr,
        gr1.department                         as Kl1_Depart,
@@ -11,7 +7,18 @@ SELECT gr1.DateSm,
        gr1.Shift                              as Kl1_Smena,
        gr1.DTStart                            as Kl1_DTStart,
        DATE_SUB(gr1.DTEnd, INTERVAL 1 SECOND) as Kl1_DTEnd,
-       gr2.*
+       gr1.Korr                               as Kl1_Korr,
+
+       gr2.DateSm                             as Kl2_DateSm,
+       gr2.clinic                             as Kl2,
+       gr2.employee                           as Kl2_Sotr,
+       gr2.department                         as Kl2_Depart,
+       gr2.post                               as Kl2_post,
+       gr2.Role                               as Kl2_Role,
+       gr2.Shift                              as Kl2_Smena,
+       gr2.DTStart                            as Kl2_DTStart,
+       DATE_SUB(gr2.DTEnd, INTERVAL 1 SECOND) as Kl2_DTEnd,
+       gr2.Korr                               as Kl2_Korr
 
 
 from analyticdb.zpn_schedule as gr1
