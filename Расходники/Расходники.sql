b0b25449d2a8f169491e3a803b3be3d068d90131
Alexey_Raid <la@zoolux.com.ua>
2023-09-18 14:11:15 +0300

    Цифры показывает, нужно теперь смотреть....

--- Расходники/Расходники.sql
+++ Расходники/Расходники.sql
@@ -11,9 +11,13 @@ select de.recorder,
        de.cost,
        round(de.cost / de.quantity, 2)         as price,
        nom.unit_rest_key_storage,
+       nom.unit_base_key,
        mea.coefficient,
        de.cost / de.quantity / mea.coefficient as PurchaseBase, -- По коэфициенту высчитываем цену продажи Базовой единицы
-       wh.type_price_key
+       wh.type_price_key,
+       pr.price as PriceBase,
+       pr.price*mea.coefficient as PriceSale
+
 
 
 from analyticdb.et_decommissioned_consumables as de
@@ -26,6 +30,12 @@ from analyticdb.et_decommissioned_consumables as de
 
 -- Подтягиваем ТипыЦен по складу продажи
          left join analyticdb.et_warehouses as wh on de.warehouse_key = wh.ref_key
+
+-- Тянем Цены номенклатуры, единица Измерения - базовая
+left join analyticdb.rash_price as pr on de.consumable_key=pr.nomenclature_key
+                                                 and  wh.type_price_key = pr.price_type_key
+and de.period >= pr.DTStart and de.period < pr.DTEnd
+and nom.unit_base_key = pr.unit_base_key
 
 
 where de.active = 1
