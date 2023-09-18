03459df10ea3bd5370e2151f0f90c96360821a3e
Alexey_Raid <la@zoolux.com.ua>
2023-09-18 11:58:18 +0300

    new

---
+++ Расходники/Расходники.sql
@@ -0,0 +1,31 @@
+select de.recorder,
+       de.line_number,
+       de.period,
+       date(de.period)                         as DT,
+       de.organization_key,
+       de.warehouse_key,
+       de.recorder_type,
+       de.service_key,
+       de.consumable_key,
+       de.quantity,
+       de.cost,
+       round(de.cost / de.quantity, 2)         as price,
+       nom.unit_rest_key_storage,
+       mea.coefficient,
+       de.cost / de.quantity / mea.coefficient as PurchaseBase, -- По коэфициенту высчитываем цену продажи Базовой единицы
+       wh.type_price_key
+
+
+from analyticdb.et_decommissioned_consumables as de
+
+-- Тянем Единицу хранения остатков с каталога номенклатура
+         left join analyticdb.et_nomenclature as nom on de.consumable_key = nom.ref_key
+
+-- Тянем Коефициент с единиц измерения
+         left join analyticdb.et_measure as mea on de.consumable_key = mea.owner_key
+
+-- Подтягиваем ТипыЦен по складу продажи
+         left join analyticdb.et_warehouses as wh on de.warehouse_key = wh.ref_key
+
+
+where de.active = 1
