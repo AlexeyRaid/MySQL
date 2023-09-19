7cb37b1902fd3b1a1325d9944d8d1ffb6c24a97a
Alexey_Raid <la@zoolux.com.ua>
2023-09-19 11:33:50 +0300

    Исправлены ошибки дублирования строк (не была учтена единица)

--- Расходники/Прайс.sql
+++ Расходники/Прайс.sql
@@ -1,22 +1,18 @@
-select pr.period as DTStart,
-       nomenclature_key,
-       price_type_key,
-       pr.price,
--- Ставим дату действия прайса (дата окончания совпадает с датой следующей цены по номенклатуре и типу цен
+select pr.period                                               AS DTStart,
+       pr.nomenclature_key                                     AS nomenclature_key,
+       pr.price_type_key                                       AS price_type_key,
+       pr.price                                                AS price,
        ifnull(
-               (select pr2.period
-                from analyticdb.et_nomenclature_prices as pr2
-                where pr.period < pr2.period
-                  and pr.nomenclature_key = pr2.nomenclature_key
-                  and pr.price_type_key = pr2.price_type_key
-                order by pr2.nomenclature_key, pr2.price_type_key, pr2.period
-                limit 1), date_add(now(), interval 1 day)
-           )     as DTEnd,
-       nom.unit_base_key
-
-from analyticdb.et_nomenclature_prices as pr
-
--- Подтягиваем базовую единицу
-         left join analyticdb.et_nomenclature as nom on pr.nomenclature_key = nom.ref_key
-
+           (select pr2.period
+               from analyticdb.et_nomenclature_prices as pr2
+               where pr.period < pr2.period
+                 and pr.nomenclature_key = pr2.nomenclature_key
+                 and pr.price_type_key = pr2.price_type_key
+               order by pr2.nomenclature_key, pr2.price_type_key, pr2.period
+               limit 1),
+           current_timestamp() + interval 1 day)               AS DTEnd,
+       nom.unit_base_key                                       AS unit_base_key
+                    from (analyticdb.et_nomenclature_prices as pr
+                        left join analyticdb.et_nomenclature nom on pr.nomenclature_key = nom.ref_key and pr.measure_key = nom.unit_rest_key_storage
+                        )
 where pr.active = 1
