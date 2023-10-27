8ad5800c1c40585cbd698cf31922332eafaef13e
Alexey_Raid <la@zoolux.com.ua>
2023-10-27 15:11:03 +0300

    Сделал

--- zpr_double_emploee.sql
+++ zpr_double_emploee.sql
@@ -1,4 +1,4 @@
-select emp.ref_key, emp.fio, emp.fio_schedule
+select emp.ref_key, emp.fio, emp.fio_schedule, count(*) as doubl
 from analyticdb.gsf_employee as emp
 
  group by emp.ref_key, emp.fio, emp.fio_schedule
