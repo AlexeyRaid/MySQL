672d6cef86a492980a1238d50af0e83ed8d8a034
Alexey_Raid <la@zoolux.com.ua>
2023-10-28 00:20:56 +0300

    Дубляж из-за уровней. Распидарасило

--- zpr_Shedule+conditions.sql
+++ zpr_Shedule+conditions.sql
@@ -7,25 +7,40 @@ select
 gr.department,
        gr.post,
 
-       lev.level                                                    as level,
-       pay.rate,
+lev.level                                                    as level,
+pay.rate,
 
 -- Делаем дату смены. Берем дату начала месяца, убираем день и вставляем вместо него дату
        date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)) as DTSmen,
 
 -- Создаем ДатуВремя начала смены
-       DATE_FORMAT(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_start),
-                   '%Y-%m-%d %H:%i')                                as DTStart,
-
+       -- Если есть эта запись в корректировке - ставим ее. Если нет - оставляем "родную"
+if(cor.employee is null,
+    -- Родная запись
+   DATE_FORMAT(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_start),'%Y-%m-%d %H:%i'),
+   -- Коректированная запись
+   DATE_FORMAT(concat(cor.shift_date, " ", cor.time_start), '%Y-%m-%d %H:%i')
+) as DTStart,
 -- Создаем ДатуВремя конца смены
-       if(gr.time_start < gr.time_end,
-           -- Если смена не припадает на смену дат - просто делаем то же самое
-          date_format(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_end),
-                      '%Y-%m-%d %H:%i'),
-           -- Если смена припадает на смену дат - добавляем сутки к дате начала смены
-          date_format(date_add(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_end),
-                               interval 1 day), '%Y-%m-%d %H:%i')
-       )                                                            as DTEnd,
+           -- Если есть эта запись в корректировке - ставим ее. Если нет - оставляем "родную"
+if(cor.employee is null,
+        -- Родная запись
+   if(gr.time_start < gr.time_end,
+       -- Если смена не припадает на смену дат - просто делаем то же самое
+      date_format(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_end),
+                  '%Y-%m-%d %H:%i'),
+       -- Если смена припадает на смену дат - добавляем сутки к дате начала смены
+      date_format(date_add(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_end),
+                           interval 1 day), '%Y-%m-%d %H:%i')
+   ),
+        -- Корректированная запись
+   if(gr.time_start < gr.time_end,
+       -- Если смена не припадает на смену дат - просто делаем то же самое
+      DATE_FORMAT(concat(cor.shift_date, " ", cor.time_end), '%Y-%m-%d %H:%i'),
+       -- Если смена припадает на смену дат - добавляем сутки к дате начала смены
+      date_add(DATE_FORMAT(concat(cor.shift_date, " ", cor.time_end), '%Y-%m-%d %H:%i'), interval 1 day)
+      )
+  ) as DTEnd,
 
 -- Считаем продолжительность смены.
 
@@ -47,7 +62,8 @@ gr.department,
                                  )
                ), 2) * 3600 / 60 / 60
                                                                     as DlSmen,
-    cor.employee as Korr
+-- Вставляем метку корректировки
+if(cor.employee is not null, '1', null) as Korr
 
 
 from analyticdb.gs_schedule as gr
@@ -64,16 +80,15 @@ left join analyticdb.gs_correction as cor on
                                         and cor.shift_date >= '2023-10-01' and cor.department = 'Фронт-Офис'
 
 -- Тянем ФИО для дальнейших связей
-         left join analyticdb.gsf_employee as emp on gr.employee = emp.fio_schedule
-
+left join analyticdb.gsf_employee as emp on gr.employee = emp.fio_schedule
+#
 -- Тянем уровень на дату
          left join analyticdb.zpr_level as lev on emp.fio = lev.fio
     and gr.post = lev.post
     and DATE_FORMAT(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_start),
                     '%Y-%m-%d %H:%i') >= lev.date_from and DATE_FORMAT(concat(
                                                                                date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)),
-                                                                               " ", gr.time_start), '%Y-%m-%d %H:%i') <=
-                                                           lev.date_to
+                                                                               " ", gr.time_start), '%Y-%m-%d %H:%i') <= lev.date_to
 
 -- Тянем условия оплаты смены по дата-клиника-смена-пост-департамент-уровень
 left join analyticdb.zpr_payconditions as pay on gr.clinic = pay.clinic
@@ -92,19 +107,5 @@ left join analyticdb.zpr_payconditions as pay on gr.clinic = pay.clinic
 
 
 where date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)) >= '2023-10-01' and gr.department = 'Фронт-Офис'
+and gr.employee = 'Маліновська' and gr.day = '1'
 
--- Добавляем корректированные смены, ставя метку 1. Вьюшка старая, писанная мной давно.... но работает....
-# UNION
-# select cor.clinic,
-#        cor.department,
-#        cor.post,
-#        cor.role,
-#        cor.shift,
-#        cor.employee,
-#        cor.fio,
-#        cor.shift_date,
-#        cor.DTStart,
-#        cor.DTEnd,
-#        cor.DlSmen,
-#        '1'
-# from zpn_correction as cor
