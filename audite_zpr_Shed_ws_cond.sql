dd3932b1379f6fb7ba1428ac960c51c633ec0810
Alexey_Raid <la@zoolux.com.ua>
2023-10-28 01:24:16 +0300

    Сделаль

--- audite_zpr_Shed_ws_cond.sql
+++ audite_zpr_Shed_ws_cond.sql
@@ -1,4 +1,4 @@
-select employee, fio, clinic, shift, department, post, level, DTSmen, DTStart, DTEnd, DlSmen, Korr, count(*)
+select employee, fio, clinic, shift, department, post, level, DTSmen, DTStart, DTEnd, DlSmen, Korr, count(*) as dbl
     from analyticdb.zpr_shedule_ws_conditions as sh
 group by employee, fio, clinic,shift, department, level, DTSmen, DTStart, DTEnd, DlSmen, Korr
 having count(*)>1
