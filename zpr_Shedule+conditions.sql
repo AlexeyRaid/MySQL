d266b29b3c1740f335cb21c449f209375c6a9aeb
Alexey_Raid <la@zoolux.com.ua>
2023-10-27 16:18:57 +0300

    подтянул уровни, но без ANY

--- zpr_Shedule+conditions.sql
+++ zpr_Shedule+conditions.sql
@@ -8,6 +8,7 @@ gr.department,
        gr.post,
 
        lev.level                                                    as level,
+       pay.rate,
 
 -- Делаем дату смены. Берем дату начала месяца, убираем день и вставляем вместо него дату
        date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)) as DTSmen,
@@ -49,6 +50,8 @@ gr.department,
 
 from analyticdb.gs_schedule as gr
 
+-- Корректировка!!
+
 
 -- Тянем ФИО для дальнейших связей
          left join analyticdb.gsf_employee as emp on gr.employee = emp.fio_schedule
@@ -63,7 +66,19 @@ from analyticdb.gs_schedule as gr
                                                            lev.date_to
 
 -- Тянем условия оплаты смены по дата-клиника-смена-пост-департамент-уровень
-
+left join analyticdb.zpr_payconditions as pay on gr.clinic = pay.clinic
+                                      and gr.post = gr.post
+                                      and gr.shift = pay.shift
+                                      and lev.level = pay.level
+                                      and DATE_FORMAT(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_start),'%Y-%m-%d %H:%i') >= pay.date_from
+                                      and if(gr.time_start < gr.time_end,
+                                           -- Если смена не припадает на смену дат - просто делаем то же самое
+                                          date_format(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_end),
+                                                      '%Y-%m-%d %H:%i'),
+                                           -- Если смена припадает на смену дат - добавляем сутки к дате начала смены
+                                          date_format(date_add(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_end),
+                                                               interval 1 day), '%Y-%m-%d %H:%i'))
+                                          <= pay.date_end
 
 
 where date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)) >= '2023-10-01' and gr.department = 'Фронт-Офис'
