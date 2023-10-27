03451dde4266f565b19307753d17ce9d77bee1f3
Alexey_Raid <la@zoolux.com.ua>
2023-10-27 13:02:35 +0300

    Дата смены Дата начала/конца

--- zpr_Shedule+conditions.sql
+++ zpr_Shedule+conditions.sql
@@ -12,16 +12,24 @@ if(gr.time_start < gr.time_end,
    date_format(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_end),'%Y-%m-%d %H:%i'),
            -- Если смена припадает на смену дат - добавляем сутки к дате начала смены
    date_format(date_add(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_end), interval 1 day),'%Y-%m-%d %H:%i')
-    )as DTEnd
+    )as DTEnd,
 
 -- Считаем продолжительность смены.
+timestampdiff(hour,
+                  (DATE_FORMAT(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'),gr.day)), " ", gr.time_start),'%Y-%m-%d %H:%i')),
+              (if(gr.time_start < gr.time_end,
+                  -- Если смена не припадает на смену дат - просто делаем то же самое
+                  date_format(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_end),
+                              '%Y-%m-%d %H:%i'),
+                  -- Если смена припадает на смену дат - добавляем сутки к дате начала смены
+                  date_format(date_add(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ",
+                                              gr.time_end), interval 1 day), '%Y-%m-%d %H:%i')
+               )
+              )
+             ) as DlSmen
 
 
 
 from analyticdb.gs_schedule as gr
 
 where date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'),gr.day)) >='2023-10-01' and gr.department = 'Фронт-Офис' or gr.department = 'Менеджер аптека' and gr.employee is not null
-
-and gr.shift  = 'Ночь'
-
-
