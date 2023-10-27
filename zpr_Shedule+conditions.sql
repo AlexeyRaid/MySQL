863b0992324a5dc907854057a2f31cc633477dce
Alexey_Raid <la@zoolux.com.ua>
2023-10-27 13:43:10 +0300

    Перевел длительность смены в десятичное число

--- zpr_Shedule+conditions.sql
+++ zpr_Shedule+conditions.sql
@@ -1,6 +1,6 @@
 select gr.employee,
        emp.fio,
-       gr.day, gr.time_start, gr.time_end, gr.`year_month`, gr.shift,
+       gr.day, gr.time_start, gr.time_end, gr.`year_month`, gr.shift, lev.level as level, gr.post,
 
 -- Делаем дату смены. Берем дату начала месяца, убираем день и вставляем вместо него дату
 date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'),gr.day)) as DTSmen,
@@ -17,23 +17,35 @@ if(gr.time_start < gr.time_end,
     )as DTEnd,
 
 -- Считаем продолжительность смены.
-timestampdiff(hour,
-                  (DATE_FORMAT(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'),gr.day)), " ", gr.time_start),'%Y-%m-%d %H:%i')),
-              (if(gr.time_start < gr.time_end,
-                  -- Если смена не припадает на смену дат - просто делаем то же самое
-                  date_format(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_end),
-                              '%Y-%m-%d %H:%i'),
-                  -- Если смена припадает на смену дат - добавляем сутки к дате начала смены
-                  date_format(date_add(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ",
-                                              gr.time_end), interval 1 day), '%Y-%m-%d %H:%i')
-               )
-              )
-             ) as DlSmen
+
+           format(
+               timestampdiff(hour,
+                             (DATE_FORMAT(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ",
+                                                 gr.time_start), '%Y-%m-%d %H:%i')),
+                             (if(gr.time_start < gr.time_end,
+                                 -- Если смена не припадает на смену дат - просто делаем то же самое
+                                 date_format(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ",
+                                                    gr.time_end),
+                                             '%Y-%m-%d %H:%i'),
+                                 -- Если смена припадает на смену дат - добавляем сутки к дате начала смены
+                                 date_format(date_add(concat(
+                                                              date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)),
+                                                              " ",
+                                                              gr.time_end), interval 1 day), '%Y-%m-%d %H:%i')
+                              )
+                                 )
+               ),2)*3600/60/60
+            as DlSmen
 
 from analyticdb.gs_schedule as gr
 
 
 -- Тянем ФИО для дальнейших связей
 left join analyticdb.gsf_employee as emp on gr.employee = emp.fio_schedule
+
+-- Тянем уровень на дату
+left join analyticdb.zpr_level as lev on emp.fio=lev.fio
+                                     and gr.post = lev.post
+                                     and DATE_FORMAT(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'),gr.day)), " ", gr.time_start),'%Y-%m-%d %H:%i') >= lev.date_from and DATE_FORMAT(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'),gr.day)), " ", gr.time_start),'%Y-%m-%d %H:%i') <= lev.date_to
 
 where date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'),gr.day)) >='2023-10-01' and gr.department = 'Фронт-Офис' or gr.department = 'Менеджер аптека' and gr.employee is not null
