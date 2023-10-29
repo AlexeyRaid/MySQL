ec87e34546a39b217f5ee7a3853b77a4b39273b0
Alexey_Raid <la@zoolux.com.ua>
2023-10-29 22:48:47 +0200

    Убрал распиздячиние

--- Оплата Регистратуры 3.0/zpr_chek_money.sql
+++ Оплата Регистратуры 3.0/zpr_chek_money.sql
@@ -1,8 +1,8 @@
-select ch.date, ch.responsible_key, emp.fio, ch.amount+ch.amount_cashless as Suum,
+select  ch.ref_key, ch.date, ch.responsible_key, emp.fio, ch.amount+ch.amount_cashless as Suum,
 
 -- Тянем с графика Post. Если подтянулся - значит был в графике. Если не подтянулся - значит Самозванец
-ifnull (gr.post, 0) as post
--- lev.level
+ifnull (gr.post, 0) as post,
+lev.level
 
 
 from analyticdb.et_money_check as ch
@@ -12,12 +12,16 @@ left join analyticdb.gsf_employee as emp on ch.responsible_key = emp.ref_key
 
 -- Определяем, это смена, или самозванец
 left join analyticdb.zpr_shedule_ws_conditions as gr on ch.date >= gr.DTStart and ch.date < gr.DTEnd
+                                                and  ch.responsible_key = gr.ref_key
 
-# -- Тянем уровень сотрудника на Дату
-# left join analyticdb.zpr_level as lev on ch.responsible_key = emp.ref_key
-#                                 and gr.post = lev.post
-#                                 and ch.date >= lev.date_from and ch.date < lev.date_to
+-- Тянем уровень сотрудника на Дату
+left join analyticdb.zpr_level as lev on ch.responsible_key = emp.ref_key
+                                 and gr.post = lev.post
+                                 and ch.date >= lev.date_from and ch.date < lev.date_to
 
 
 where ch.date >= '2023-10-01' and ch.is_posted = 1
+
+
   and emp.fio is not null
+group by(ch.ref_key)
