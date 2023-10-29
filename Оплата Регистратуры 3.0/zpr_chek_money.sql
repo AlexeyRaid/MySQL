67f5ed2419ba4f0bb7cc9ee293d6c871fbaf6734
Alexey_Raid <la@zoolux.com.ua>
2023-10-29 19:35:58 +0200

    Метки Пост и Самозванец

--- Оплата Регистратуры 3.0/zpr_chek_money.sql
+++ Оплата Регистратуры 3.0/zpr_chek_money.sql
@@ -1,7 +1,7 @@
 select ch.date, ch.responsible_key, emp.fio, ch.amount+ch.amount_cashless as Suum,
 
--- Если с графика не подтянулся - ставим 1, если не подтянулся - 0
-if (gr.DTSmen is null , 1,0) as Samozv
+-- Тянем с графика Post. Если подтянулся - значит был в графике. Если не подтянулся - значит Самозванец
+ifnull (gr.post, 0) as post
 
 
 from analyticdb.et_money_check as ch
@@ -11,5 +11,6 @@ left join analyticdb.gsf_employee as emp on ch.responsible_key = emp.ref_key
 
 -- Определяем, это смена, или самозванец
 left join analyticdb.zpr_shedule_ws_conditions as gr on ch.date >= gr.DTStart and ch.date < gr.DTEnd
+
 
 where ch.date >= '2023-10-01' and ch.is_posted = 1
