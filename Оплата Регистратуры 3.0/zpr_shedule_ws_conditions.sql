29124776d818122b08a80d0c70a79b7e633dae23
Alexey_Raid <la@zoolux.com.ua>
2023-10-29 18:58:49 +0200

    Добавлена ставка в час и ЗП фактическая по отработанным часам

--- Оплата Регистратуры 3.0/zpr_shedule_ws_conditions.sql
+++ Оплата Регистратуры 3.0/zpr_shedule_ws_conditions.sql
@@ -6,7 +6,52 @@ select
     gr.department,
     gr.post,
     lev.level as level,
-    COALESCE(pay.rate, pay2.rate) as Rate,
+    COALESCE(pay.rate, pay2.rate)/12 as RateInHour, -- Ставка в час
+    COALESCE(pay.rate, pay2.rate)/12 * -- Ставка за смену
+        -- Считаем продолжительность смены.
+format(
+        timestampdiff(hour,
+            -- Создаем ДатуВремя начала смены
+            -- Если есть эта запись в корректировке - ставим ее. Если нет - оставляем "родную"
+                      if(cor.employee is null,
+                          -- Родная запись
+                         DATE_FORMAT(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ",
+                                            gr.time_start), '%Y-%m-%d %H:%i'),
+                          -- Коректированная запись
+                         DATE_FORMAT(concat(cor.shift_date, " ", cor.time_start), '%Y-%m-%d %H:%i')),
+            -- Создаем ДатуВремя конца смены
+            -- Если есть эта запись в корректировке - ставим ее. Если нет - оставляем "родную"
+                      if(cor.employee is null,
+                          -- Родная запись
+                         if(gr.time_start < gr.time_end,
+                             -- Если смена не припадает на смену дат - просто делаем то же самое
+                            date_format(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ",
+                                               gr.time_end),
+                                        '%Y-%m-%d %H:%i'),
+                             -- Если смена припадает на смену дат - добавляем сутки к дате начала смены
+                            date_format(date_add(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)),
+                                                        " ", gr.time_end),
+                                                 interval 1 day), '%Y-%m-%d %H:%i')
+                         ),
+                          -- Корректированная запись
+                         if(gr.time_start < gr.time_end,
+                             -- Если смена не припадает на смену дат - просто делаем то же самое
+                            DATE_FORMAT(concat(cor.shift_date, " ", cor.time_end), '%Y-%m-%d %H:%i'),
+                             -- Если смена припадает на смену дат - добавляем сутки к дате начала смены
+                            date_add(DATE_FORMAT(concat(cor.shift_date, " ", cor.time_end), '%Y-%m-%d %H:%i'), interval
+                                     1 day)
+                         )
+                      )
+                     )
+                    ,
+                             (if(gr.time_start < gr.time_end,
+                                 -- Если смена не припадает на смену дат - просто делаем то же самое
+                                 date_format(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_end), '%Y-%m-%d %H:%i'),
+                                 -- Если смена припадает на смену дат - добавляем сутки к дате начала смены
+                                 date_format(date_add(concat(date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)), " ", gr.time_end), interval 1 day), '%Y-%m-%d %H:%i')
+                                 )
+                            )
+               , 2) * 3600 / 60 / 60 as Rate_Fkt,
 
 -- Делаем дату смены. Берем дату начала месяца, убираем день и вставляем вместо него дату
 date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)) as DTSmen,
@@ -189,4 +234,3 @@ left join analyticdb.gsf_employee as emp on gr.employee = emp.fio_schedule
                                           <= pay2.date_end
 
 where date(concat(DATE_FORMAT(gr.`year_month`, '%Y-%m-'), gr.day)) >= '2023-10-01' and gr.department = 'Фронт-Офис' and gr.employee is not null
-
