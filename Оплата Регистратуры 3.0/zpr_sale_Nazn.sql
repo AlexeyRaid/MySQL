932beb0c8bd941b19e159e02c0182c97b003047c
Alexey_Raid <la@zoolux.com.ua>
2023-10-31 21:04:25 +0200

    навел дезигн

--- Оплата Регистратуры 3.0/zpr_sale_Nazn.sql
+++ Оплата Регистратуры 3.0/zpr_sale_Nazn.sql
@@ -1,17 +1,17 @@
-select s.recorder, s.line_number, s.period, s.organization_key, s.employee, s.price-s.amount_of_costs as VP,
-       emp.fio, cl.clinic,
-        s.organization_key,
-        gr.shift,
+select s.recorder,
+       s.line_number,
+       s.period, s.organization_key,
+       cl.clinic,
+       s.employee,
+       emp.fio,
+       s.price-s.amount_of_costs as VP,
+       gr.shift,
         ifnull (gr.post, 0) as post, -- Тянем с графика Post. Если подтянулся - значит был в графике. Если не подтянулся - значит Самозванец
         gr.level, -- Тут все должно работать, но нюанс - уровень тянется не с условий, а с графика. Но так как мы один хрен джойним график, грех не взять оттуда левел. Смысл в еще одном джойне?
         coalesce(pay.part_prod_assign, pay2.part_prod_assign) as Prem,
         coalesce(pay.part_prod_assign, pay2.part_prod_assign)*(s.price-s.amount_of_costs) as ZP
 
-
 from analyticdb.et_sales as s
-
-
-
 
 -- Тянем аналитику по ЗП
 left join analyticdb.et_nomenclature as n on s.nomenclature_key = n.ref_key
@@ -39,8 +39,5 @@ left join analyticdb.zpr_payconditions as pay2 on 'ANY' = pay2.clinic
                                         and gr.post = pay2.post
                                         and gr.shift = pay2.shift
                                         and s.period >= pay2.date_from and s.period <= pay2.date_end
-
-
-
 
 where s.period >= '2023-10-01' and n.salary_analytics_key = '4cf516b4-17bf-11e3-5888-08606e6953d2' and s.active = 1
